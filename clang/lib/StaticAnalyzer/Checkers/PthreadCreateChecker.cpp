5dc5a89ed316799b680bff4847d4814052511e82
ibn222 <ibn222@eid.utexas.edu>
2024-11-12 09:58:04 -0800

    Create pthread_create(3) checker

---
+++ clang/lib/StaticAnalyzer/Checkers/PthreadCreateChecker.cpp
@@ -0,0 +1,28 @@
+//===--- PthreadCreateChecker.cpp - Prevent pthreads from escaping analysis ---*- C++ -*--===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#include "clang/StaticAnalyzer/Checkers/BuiltinCheckerRegistration.h"
+#include "clang/StaticAnalyzer/Core/BugReporter/BugType.h"
+#include "clang/StaticAnalyzer/Core/Checker.h"
+#include "clang/StaticAnalyzer/Core/PathSensitive/CallDescription.h"
+#include "clang/StaticAnalyzer/Core/PathSensitive/CallEvent.h"
+#include "clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h"
+
+using namespace clang;
+using namespace ento;
+
+class PthreadCreateChecker : public Checker<check::PostCall> {
+};
+
+
+void ento::registerPthreadCreateChecker(CheckerManager &mgr) {
+  mgr.registerChecker<PthreadCreateChecker>();
+}
